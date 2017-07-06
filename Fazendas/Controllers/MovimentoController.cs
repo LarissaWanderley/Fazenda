aab026b0cac503cf30656060bca75c7caf5f4bff
LarissaWanderley <lmwjogos@gmail.com>
2017-07-06 01:38:35 -0300

    modelos tela

---
+++ Fazendas/Controllers/MovimentoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fazendas.Controllers
+{
+    public class MovimentoController : Controller
+    {
+        // GET: Movimento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fazendas/Persistencia/DBGrupo.cs
@@ -0,0 +1,12 @@
+﻿using Fazendas.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fazendas.Persistencia
+{
+    public class DBGrupo : DBBroker.Engine.DBBroker<Grupo>
+    {
+    }
+}
